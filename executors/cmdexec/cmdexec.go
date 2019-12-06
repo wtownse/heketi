1a93a452e45f777966750b258c5796b8cefaeb12
Townsend, Wayne <Wayne.Townsend@emc.com>
2019-12-06 15:40:19 -0500

    cmdexec: run udevadm through the LVM wrapper as well
    The udev devices might only be available on the container host, and not
    inside the container where Gluster is running.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- executors/cmdexec/cmdexec.go
+++ executors/cmdexec/cmdexec.go
@@ -66,6 +66,9 @@ func (c *CmdExecutor) lvmCommand() string {
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 95a94eaf... cmdexec: run udevadm through the LVM wrapper as well
 func (c *CmdExecutor) udevCommand() string {
 	if wrapper := c.LVMWrapper(); wrapper != "" {
 		return wrapper + " /usr/bin/udevadm"
@@ -74,8 +77,11 @@ func (c *CmdExecutor) udevCommand() string {
 	return "/usr/bin/udevadm"
 }
 
+<<<<<<< HEAD
 =======
 >>>>>>> 68f78cde... cmdexec: add lvmCommand() and LVMWrapper()
+=======
+>>>>>>> 95a94eaf... cmdexec: run udevadm through the LVM wrapper as well
 func setWithEnvVariables(config *CmdConfig) {
 	var env string
 
